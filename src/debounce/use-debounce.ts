5c5fbdec8100162a80055b5454d587d2b5cd9d33
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-08-26 22:00:40 +0300

    added return type to the hook

--- src/debounce/use-debounce.ts
+++ src/debounce/use-debounce.ts
@@ -2,7 +2,10 @@ import { useEffect } from "react";
 
 import { debounce } from "./debounce";
 
-export const useDebounce = <A = unknown, R = void>(fn: (args: A) => R, ms: number) => {
+export const useDebounce = <A = unknown, R = void>(
+    fn: (args: A) => R,
+    ms: number
+): ((args: A) => Promise<R>) => {
     const [debouncedFun, cancel] = debounce<A, R>(fn, ms);
 
     useEffect(() => () => cancel());
