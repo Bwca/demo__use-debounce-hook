b63c4cbacd85d92cd09929453fbdd4ec12879b1f
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-08-26 22:08:21 +0300

    changed teardown name

--- src/debounce/use-debounce.ts
+++ src/debounce/use-debounce.ts
@@ -6,9 +6,9 @@ export const useDebounce = <A = unknown, R = void>(
     fn: (args: A) => R,
     ms: number
 ): ((args: A) => Promise<R>) => {
-    const [debouncedFun, cancel] = debounce<A, R>(fn, ms);
+    const [debouncedFun, teardown] = debounce<A, R>(fn, ms);
 
-    useEffect(() => () => cancel());
+    useEffect(() => () => teardown(), []);
 
     return debouncedFun;
 };
